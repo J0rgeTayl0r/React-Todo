f10a844ae61f3165a30a8769c6078cbcc8c3a3c9
carlo rothschild <carlorothschild@thors-hammer.home>
2020-05-04 18:53:35 -0400

    MVP

--- src/TodoComponents/Todo.js
+++ src/TodoComponents/Todo.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Todo = props => {
     return (
         <div
+            style={props.todo.completed ? {textDecoration:'line-through'} : null}
             onClick={() => { props.toggleCompleted(props.todo.id) }}
             className={props.todo.completed ?
                 'completed' : 'list-item'}
