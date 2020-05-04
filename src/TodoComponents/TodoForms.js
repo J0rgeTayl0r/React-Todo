db3d2cb562b4a907c9ea8d13989e7cb00ddbe32e
carlo rothschild <carlorothschild@thors-hammer.home>
2020-05-04 18:04:14 -0400

    about to add toggle

--- src/TodoComponents/TodoForms.js
+++ src/TodoComponents/TodoForms.js
@@ -1,27 +1,25 @@
-import React from 'react'
+import React from "react";
 
+const TodoForm = props => {
+  return (
+    <form>
+      <input
+        className="form"
+        type="text"
+        value={props.inputText}
+        placeholder="Whats todays plan?"
+        onChange={props.handleChanges}
+        name="inputText"
+      />
+      <button className="btn-add" onClick={props.addNewItem}>
+        Add Something
+      </button>
 
-const TodoForm = (props) => {
-    return (
-        <form>
-            <input
-                className='form'
-                type='text'
-                value={props.inputText}
-                placeholder='Whats todays plan?'
-                onChange={props.handleChanges}
-                name='inputText'
-            />
-            <button className='btn-add'
-                    onClick={props.addNewItem}           
-            >Add Something</button>
-
-            <button
-                className='btn-rmv'
-                onClick={props.removeSelected}
-            >Remove it</button>
-        </form>
-    )
-}
+      <button className="btn-rmv" onClick={props.removeSelected}>
+        Remove it
+      </button>
+    </form>
+  );
+};
 
 export default TodoForm;
